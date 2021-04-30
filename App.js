774d8fc6da22ee674dc401da64ac479578d3042f
Jose de Jesus Gamboa Contreras <jesus.gamboa@tya.com.mx>
2021-04-30 09:22:02 -0500

    Begin

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Icon.Button
+    name="facebook"
+    backgroundColor="#3b5998"
+    
+  >
+    Login with Facebook
+  </Icon.Button>
       <StatusBar style="auto" />
     </View>
   );
